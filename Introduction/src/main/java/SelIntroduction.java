fa6d0ab4b8153f3e7e822741b4ab50d416fed991
Forrest JB Hartley <forrest@forrestbear.org>
2022-03-13 13:12:09 -0400

    Added introductory lesson sample

---
+++ Introduction/src/main/java/SelIntroduction.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelIntroduction {
+    public static void main(String[] args) {
+        // Invoke Browser
+        System.setProperty("webdriver.chrome.driver","/opt/homebrew/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://forrestbear.org");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+    }
+}
