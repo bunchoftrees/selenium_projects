bcbd9ba8d0b3db33277be3054104d27043517daa
Forrest JB Hartley <forrest@forrestbear.org>
2022-03-13 14:56:27 -0400

    added primitive variable class file

---
+++ JavaCrashCourse/src/main/java/CoreJavaVariables.java
@@ -0,0 +1,17 @@
+public class CoreJavaVariables {
+    public static void main(String[] args) {
+        int myNum = 5;
+        String myWebsite = "https://forrestbear.org";
+        char myLetter = 'A';
+        double myDecimal = 3.14;
+        boolean myBool = true;
+
+        System.out.printf("myNum: %s\nmyWebsite: %s\nmyLetter: %s\nmyDecimal: %s\nmyBool: %s",
+                myNum,
+                myWebsite,
+                myLetter,
+                myDecimal,
+                myBool);
+
+    }
+}
